8b8eb0fa5bfcfcb0a3cb8586950c53df8fd7cc6d
antonio <antonio.popa.dev@gmail.com>
2025-07-03 16:11:51 +0300

    Update

--- src/services/api.ts
+++ src/services/api.ts
@@ -36,58 +36,58 @@ class ApiClient {
     );
 
     // Response interceptor to handle token refresh
-    // this.client.interceptors.response.use(
-    //   (response) => response,
-    //   async (error) => {
-    //     const originalRequest = error.config;
+    this.client.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error.config;
 
-    //     if (error.response?.status === 401 && !originalRequest._retry) {
-    //       if (this.isRefreshing) {
-    //         // If we're already refreshing, queue this request
-    //         return new Promise((resolve, reject) => {
-    //           this.failedQueue.push({ resolve, reject });
-    //         }).then((token) => {
-    //           originalRequest.headers.Authorization = `Bearer ${token}`;
-    //           return this.client(originalRequest);
-    //         }).catch((err) => {
-    //           return Promise.reject(err);
-    //         });
-    //       }
+        if (error.response?.status === 401 && !originalRequest._retry) {
+          if (this.isRefreshing) {
+            // If we're already refreshing, queue this request
+            return new Promise((resolve, reject) => {
+              this.failedQueue.push({ resolve, reject });
+            }).then((token) => {
+              originalRequest.headers.Authorization = `Bearer ${token}`;
+              return this.client(originalRequest);
+            }).catch((err) => {
+              return Promise.reject(err);
+            });
+          }
 
-    //       originalRequest._retry = true;
-    //       this.isRefreshing = true;
+          originalRequest._retry = true;
+          this.isRefreshing = true;
 
-    //       try {
-    //         const refreshToken = localStorage.getItem('refreshToken');
-    //         if (!refreshToken) {
-    //           throw new Error('No refresh token available');
-    //         }
+          try {
+            const refreshToken = localStorage.getItem('refreshToken');
+            if (!refreshToken) {
+              throw new Error('No refresh token available');
+            }
 
-    //         const response = await this.client.post('/auth/refresh', {
-    //           refreshToken,
-    //         });
+            const response = await this.client.post('/auth/refresh', {
+              refreshToken,
+            });
 
-    //         const { accessToken } = response.data;
-    //         localStorage.setItem('accessToken', accessToken);
+            const { accessToken } = response.data;
+            localStorage.setItem('accessToken', accessToken);
 
-    //         // Process the failed queue
-    //         this.processQueue(null, accessToken);
+            // Process the failed queue
+            this.processQueue(null, accessToken);
 
-    //         originalRequest.headers.Authorization = `Bearer ${accessToken}`;
-    //         return this.client(originalRequest);
-    //       } catch (refreshError) {
-    //         this.processQueue(refreshError, null);
-    //         this.clearTokens();
-    //         window.location.href = '/login';
-    //         return Promise.reject(refreshError);
-    //       } finally {
-    //         this.isRefreshing = false;
-    //       }
-    //     }
+            originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+            return this.client(originalRequest);
+          } catch (refreshError) {
+            this.processQueue(refreshError, null);
+            this.clearTokens();
+            window.location.href = '/login';
+            return Promise.reject(refreshError);
+          } finally {
+            this.isRefreshing = false;
+          }
+        }
 
-    //     return Promise.reject(error);
-    //   }
-    // );
+        return Promise.reject(error);
+      }
+    );
   }
 
   private processQueue(error: any, token: string | null) {
